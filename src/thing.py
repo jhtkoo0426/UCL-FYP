1432bbb0bbea734cc68354b8d153e7204300283c
jhtkoo0426 <jhtkoo@gmail.com>
2023-03-03 16:16:49 +0000

    Refactored simulation to unit processes

--- src/thing.py
+++ src/thing.py
@@ -2,21 +2,12 @@
 Class for defining & initializing object meshes in Pybullet simulations.
 """
 
-
 import pybullet as p
 import os
 import pybullet_data
 
 
-class Thing:     
-
-# # neccesary parameters of the mug, the global scaling is used to shrink the size of the mug
-# mug:
-#   object_name: Mug
-#   object_position: [0.7, 0, 0.0]
-#   global_scaling : 0.5
-
-
+class Thing:
     def __init__(self, name, position, globalScaling):
 
         self.name = name
@@ -27,38 +18,13 @@ class Thing:
         """
         for better organization, should move the urdf path to the arg85 yaml file as well...
         """
-        self.urdf_path = "./src/urdf/objects/mug/mug.urdf"
-        # self.urdf_path = "./src/urdf/objects/block.urdf"
+        # self.urdf_path = "./src/urdf/objects/mug/mug.urdf"
+        self.urdf_path = "./src/urdf/objects/block.urdf"
         # self.urdf_path = "./src/urdf/objects/bleach_cleanser/model.urdf"
 
-        print(f"Created a {name}")
+        id = p.loadURDF(self.urdf_path, self.initPos, self.initOrientation, useFixedBase=False, globalScaling=self.objectScale)
+        self.setID(id)
 
-        if name=="Mug":
-            id = p.loadURDF(self.urdf_path, self.initPos, self.initOrientation, useFixedBase=False, globalScaling = self.objectScale)
-            print(f'Load {name}')
-            print(f"Set ID to {id}")
-            self.setID(id)
-
-
-        # elif name=="Bottle":
-        #     self.objShapeID = p.createVisualShape(shapeType=p.GEOM_MESH, fileName=address, 
-        #                             rgbaColor=[1, 0, 0, 1], specularColor=[0.4, .4, 0], 
-        #                             visualFramePosition=shift, meshScale=meshScale)
-        #     self.objCollisionID = p.createCollisionShape(shapeType=p.GEOM_MESH, fileName=address, 
-        #                                 collisionFramePosition=shift, meshScale=meshScale)
-        #     if onObject is not None:
-        #         if onObject.name == "table"  :
-        #             self.initPos[2] += onObject.getHeight()
-
-        #     self.ID = p.createMultiBody(baseMass=0.2,
-        #                         baseInertialFramePosition=inertial,
-        #                         baseCollisionShapeIndex=self.objCollisionID,
-        #                         baseVisualShapeIndex=self.objShapeID,
-        #                         basePosition=self.initPos,
-        #                         baseOrientation=ObjIniOrientation,
-        #                         useMaximalCoordinates=True)
-            
-        #     print(f"Set ID to {self.ID}") 
 
     def setInitPos(self,pos):
         self.initPos=pos
